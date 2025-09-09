f1e8a52dbdb393347fb47aafbdc330755e99a8f9
羽瀬川拓人 <orternoom@gmail.com>
2025-09-10 00:05:23 +0900

    20250910

--- app/components/Badge.vue
+++ app/components/Badge.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="inline-block w-[42px] h-[24px] border border-1 border-[#F67A67] rounded-md pb-[6px] text-center">
+    <div class="inline-block w-[42px] border border-1 border-[#F67A67] rounded-md pb-[2px] text-center">
         <span class="text-[14px] text-[#F67A67] font-mincho"><slot></slot></span>
     </div>
 </template>
