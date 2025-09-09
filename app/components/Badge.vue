654866e21de1b37c17caec800f9612cffdbd8167
羽瀬川拓人 <orternoom@gmail.com>
2025-09-09 23:49:46 +0900

    Update Badge.vue

--- app/components/Badge.vue
+++ app/components/Badge.vue
@@ -1,3 +1,5 @@
 <template>
-    <span class="border border-1 border-[#F67A67] rounded-md px-[7px] pt-[1px] pb-[3px] box-border text-[#F67A67] font-mincho"><slot></slot></span>
+    <div class="inline-block w-[42px] h-[24px] border border-1 border-[#F67A67] rounded-md pb-[6px] text-center">
+        <span class="text-[14px] text-[#F67A67] font-mincho"><slot></slot></span>
+    </div>
 </template>
